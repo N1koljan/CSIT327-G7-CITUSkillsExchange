0d94e4a58091f50d2375559aeb83f1143ad69fe6
letjungcook7 <mgerardgrant@yahoo.com>
2025-12-01 21:26:53 +0800

    Deployment Fix Attempt v1

--- skills_exchange/settings.py
+++ skills_exchange/settings.py
@@ -126,7 +126,7 @@ else:
     DATABASES = {
         'default': dj_database_url.config(
             default=DATABASE_URL,
-            conn_max_age=600,
+            conn_max_age=0,
             ssl_require=True
         )
     }
